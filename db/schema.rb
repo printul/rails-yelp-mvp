ccaac26ad90044c497d80839ce52a67cbf198699
Catalin Rusu <printul@gmail.com>
2017-05-24 12:41:09 +0100

    added phone number to restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524112228) do
+ActiveRecord::Schema.define(version: 20170524113953) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170524112228) do
     t.string   "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "phone"
   end
 
   create_table "reviews", force: :cascade do |t|
