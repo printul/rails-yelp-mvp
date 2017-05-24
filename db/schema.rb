78fb6ab26348562588a05cc21cecb0da08b680b7
Catalin Rusu <printul@gmail.com>
2017-05-24 12:46:13 +0100

    changed col name to phone_number

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524113953) do
+ActiveRecord::Schema.define(version: 20170524114413) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
     t.string   "category"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "phone"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "phone_number"
   end
 
   create_table "reviews", force: :cascade do |t|
