60421fd297a36f0e6711c5078b8239b563ed61f9
Catalin Rusu <printul@gmail.com>
2017-05-24 12:30:16 +0100

    added validation for restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,6 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews, dependent: destroy
+  has_many :reviews, dependent: :destroy
+  validates :rating, inclusion: { in: [0,1,2,3,4,5], allow_nil: false }
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
 end
